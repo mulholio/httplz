d156654c0c44f5fe2e019949898f34a0bbc3f0c4
James Mulholland <james@jmulholland.com>
2021-07-31 13:44:44 +0100

    print out basic status code info from a map

--- httplz.go
+++ httplz.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+type codeNumber = int16
+type statusCode struct {
+	Code              codeNumber
+	Name, Description string
+}
+
+func (sc statusCode) Format() string {
+	return fmt.Sprintf("\n%d: %s\n\n%s", sc.Code, sc.Name, sc.Description)
+}
+
+var codes = map[codeNumber]statusCode{
+	400: statusCode{Code: 400, Name: "Bad Request", Description: "The HyperText Transfer Protocol (HTTP) 400 Bad Request response status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing)."},
+}
+
 func main() {
-	fmt.Println("Hello world")
+	// TODO flags to read
+	c := codes[400]
+	fmt.Println(c.Format())
 }
